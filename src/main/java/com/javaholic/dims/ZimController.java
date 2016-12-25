0da46418932e9d11b4314f798db937c9aa960b45
DESKTOP-OD415VB\leehyungyu <gusrb0808@naver.com>
2016-12-25 22:54:51 +0900

    MySQL커넥터 추가 및 데이터베이스 연결

--- src/main/java/com/javaholic/dims/ZimController.java
+++ src/main/java/com/javaholic/dims/ZimController.java
@@ -4,8 +4,10 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class ZimController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(ZimController.class);
+	
+	@Autowired
+	SqlSessionTemplate s;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
